4b19d403e25db4ffd3f3a172b750434fb8e387a3
Amarnadhkumar <102464997+Amarnadhkumar@users.noreply.github.com>
2022-05-21 11:43:00 +0530

    Delete af3.js

--- af3.js
+++
@@ -1,9 +0,0 @@
-let x1 = function () {  
-    console.log('It is an anonymous function');  
-};  
-x1();
-
-
-setTimeout(function () {  
-    console.log('Execute later after 5 second')  
-}, 1000);  
